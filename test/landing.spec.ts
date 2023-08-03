a780cd19207d8dd1035b0b3d2315530fa25e7bdf
dmi <dmi.villione@gmail.com>
2023-08-03 14:00:05 +0300

    update workflow

--- test/landing.spec.ts
+++ test/landing.spec.ts
@@ -7,17 +7,16 @@ expect.extend({ toMatchImageSnapshot })
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-const takeScreenshot = async (dir: string, dirName: string) => {
-	const filename = "screen"
-	expect(
-		await page.screenshot({
-			fullPage: false,
-			path: `./app_snapshots/${dirName}/received/${dir}/${filename}-snap.png`,
-		})
-	).toMatchImageSnapshot({
-		customSnapshotIdentifier: filename,
-		customSnapshotsDir: `./app_snapshots/${dirName}/base/` + dir,
-		customDiffDir: `./app_snapshots/${dirName}/diff/` + dir,
+const takeScreenshot = async (
+	dirName: string,
+	fileName: string,
+	snapshot: Buffer
+) => {
+	expect(snapshot).toMatchImageSnapshot({
+		customSnapshotIdentifier: fileName,
+
+		customSnapshotsDir: `./app_snapshots/${dirName}/base/`,
+		customDiffDir: `./app_snapshots/${dirName}/diff/`,
 		failureThreshold: 0.005,
 		failureThresholdType: "percent",
 	})
@@ -33,17 +32,47 @@ const openPage = async (page: Page, url: string) => {
 }
 type TestCfg = { url: string; delay: string }[]
 
+const readDirToList = (path: string) => {
+	try {
+		return fs.readdirSync(path)
+	} catch {
+		return []
+	}
+}
+
 describe("Test", () => {
 	const cfg = fs.readFileSync(process.env.CONFIG_PATH, "utf8")
 	const testCfg = JSON.parse(cfg) as TestCfg
 
 	for (let test of testCfg) {
-		console.log("CONFIG:", test)
+		const hash = getHash(test.url)
+		const baseSnapshots = readDirToList(`./app_snapshots/${hash}/base`)
+
 		it("First page", async () => {
+			let failsCounts = 0
 			await openPage(page, test.url)
 			await page.waitForLoadState("domcontentloaded")
 			await sleep(parseInt(test.delay || "2000"))
-			await takeScreenshot(`${deviceName}`, getHash(test.url))
+			const currScreen = await page.screenshot({
+				fullPage: false,
+				path: `./app_snapshots/${hash}/received/screen-${String(
+					Math.random()
+				).slice(2)}-snap.png`,
+			})
+			for (let screenName of baseSnapshots) {
+				try {
+					await takeScreenshot(
+						getHash(test.url),
+						screenName.replace("-snap.png", ""),
+						currScreen
+					)
+				} catch {
+					failsCounts++
+				}
+			}
+			if (failsCounts === baseSnapshots.length)
+				throw new Error("snapshots diff found")
+			process.exit(0)
 		})
 	}
 })
