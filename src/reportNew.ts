2bb3036ee92c583bbbba6a1d580a5a84970f86bc
dmi <dmi.villione@gmail.com>
2023-08-07 18:38:51 +0300

    fix errors

--- src/reportNew.ts
+++ src/reportNew.ts
@@ -7,26 +7,29 @@ const SLACK_CHANNEL = process.env.SLACK_CHANNEL || ""
 ;(async () => {
 	const { ts } = await slackClient.chat.postMessage({
 		channel: SLACK_CHANNEL,
-		text: `*Я.Лендинг*\n:white_check_mark: Первый запуск тестов прошёл успешно. Скриншоты отправлены <${process.env.PULL_REQUEST_URL}|пулл реквестом>`
+		text: `*Я.Лендинг*\n:white_check_mark: Первый запуск тестов прошёл успешно. Скриншоты отправлены <${process.env.PULL_REQUEST_URL}|пулл реквестом>`,
 	})
-	const newFilesList = fs.readFileSync(path.join(__dirname, "../app_snapshots/newFiles"), {"encoding":"utf-8"}).toString().split(os.EOL)
+	const newFilesList = fs
+		.readFileSync(path.join(__dirname, "../app_snapshots/newFiles"), {
+			encoding: "utf-8",
+		})
+		.toString()
+		.split(os.EOL)
 	const configMap = JSON.parse(
 		fs.readFileSync(path.join(__dirname, "../test_config.map.json"), "utf8")
 	) as Record<string, string>
 
 	for (let file of newFilesList) {
 		const url = configMap[file.split("/")[1]]
-		
+
 		await slackClient.files.upload({
 			channels: SLACK_CHANNEL,
 			thread_ts: ts,
-			filename:  path.extname(file),
+			filename: path.extname(file),
 			file: fs.createReadStream(
-				path.join(
-					__dirname,
-					`../app_snapshots/${file}`
-				)
+				path.join(__dirname, `../app_snapshots/${file}`)
 			),
 			initial_comment: `${url}`,
-		}
+		})
+	}
 })()
