eb5e21c102dfc560e8297c9121b2755240cf1d14
dmi <dmi.villione@gmail.com>
2023-08-07 18:34:27 +0300

    add new files logs

--- src/reportNew.ts
+++ src/reportNew.ts
@@ -1,4 +1,7 @@
 import { slackClient } from "./slack"
+import fs from "fs"
+import path from "path"
+import os from "os"
 
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL || ""
 ;(async () => {
@@ -6,4 +9,24 @@ const SLACK_CHANNEL = process.env.SLACK_CHANNEL || ""
 		channel: SLACK_CHANNEL,
 		text: `*Я.Лендинг*\n:white_check_mark: Первый запуск тестов прошёл успешно. Скриншоты отправлены <${process.env.PULL_REQUEST_URL}|пулл реквестом>`
 	})
+	const newFilesList = fs.readFileSync(path.join(__dirname, "../app_snapshots/newFiles"), {"encoding":"utf-8"}).toString().split(os.EOL)
+	const configMap = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "../test_config.map.json"), "utf8")
+	) as Record<string, string>
+
+	for (let file of newFilesList) {
+		const url = configMap[file.split("/")[1]]
+		
+		await slackClient.files.upload({
+			channels: SLACK_CHANNEL,
+			thread_ts: ts,
+			filename:  path.extname(file),
+			file: fs.createReadStream(
+				path.join(
+					__dirname,
+					`../app_snapshots/${file}`
+				)
+			),
+			initial_comment: `${url}`,
+		}
 })()
