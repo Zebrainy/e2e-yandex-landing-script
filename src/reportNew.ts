8481b9121858c1d8915b3cd0b9dd7d4eea6b7837
dmi <dmi.villione@gmail.com>
2023-08-07 18:43:24 +0300

    rename msg

--- src/reportNew.ts
+++ src/reportNew.ts
@@ -7,7 +7,7 @@ const SLACK_CHANNEL = process.env.SLACK_CHANNEL || ""
 ;(async () => {
 	const { ts } = await slackClient.chat.postMessage({
 		channel: SLACK_CHANNEL,
-		text: `*Я.Лендинг*\n:white_check_mark: Первый запуск тестов прошёл успешно. Скриншоты отправлены <${process.env.PULL_REQUEST_URL}|пулл реквестом>`,
+		text: `*Я.Лендинг*\n:white_check_mark: Появились новые скриншоты. <${process.env.PULL_REQUEST_URL}|пулл реквест>`,
 	})
 	const newFilesList = fs
 		.readFileSync(path.join(__dirname, "../app_snapshots/newFiles"), {
