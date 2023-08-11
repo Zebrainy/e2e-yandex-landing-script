d365694c250498b901f976a712e9455622f61840
dmi <dmi.villione@gmail.com>
2023-08-11 17:19:08 +0300

    remoe logs

--- src/clearing.ts
+++ src/clearing.ts
@@ -5,12 +5,10 @@ import path from "path"
 		fs.readFileSync(path.join(__dirname, "../test_config.map.json"), "utf8")
 	) as Record<string, string>
 
-    console.log("configMap",configMap)
 	const foldersToDelete = fs.readdirSync(
 		path.join(__dirname, "../../app_snapshots/")
 	)
 
-    console.log("foldersToDelete",foldersToDelete)
 	for (let file of foldersToDelete) {
 		if (file in configMap) continue
 
